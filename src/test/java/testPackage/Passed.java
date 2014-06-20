0c029ef734139dc9f0e4046092ffb7963a6932e4
Anatoly <poddubny.anatoly@gmail.com>
2014-06-20 21:16:33 +0300

    pustoy comment

---
+++ src/test/java/testPackage/Passed.java
@@ -0,0 +1,19 @@
+package testPackage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: admin
+ * Date: 20.06.14
+ * Time: 21:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class Passed {
+   @Test
+    public void passed(){
+
+        Assert.assertEquals(true,true);
+    }
+}
